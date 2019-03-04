3a2455a2c70cfd0b6e583e95865923d4aaf4666d
doldrums.317@gmail.com <Cheverdaarina24>
2019-03-04 21:58:41 +0200

    add StatFragment Design

--- app/src/main/java/com/example/final_project/Main22Activity.kt
+++ app/src/main/java/com/example/final_project/Main22Activity.kt
@@ -3,22 +3,20 @@ package com.example.final_project
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main22.*
+import kotlinx.android.synthetic.main.layout.*
+
 
 class Main22Activity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_solo)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_duo)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_squard)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -27,7 +25,9 @@ class Main22Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main22)
+        setContentView(R.layout.layout)
+
+        supportActionBar?.hide()
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
