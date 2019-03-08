2e612ed53bfa18f0b66688b223ed59141991660d
Anton Sharapov <cheel4069@gmail.com>
2019-03-08 13:50:51 +0200

    коммит чтобы сохранить изменения (пробую передать класс игрока в фрагмент со списком)

--- app/src/main/java/com/example/final_project/API/APIFunctions.kt
+++ app/src/main/java/com/example/final_project/API/APIFunctions.kt
@@ -1,9 +1,9 @@
 package com.example.final_project.API
 
-import android.content.Context
 import android.util.Log
 import com.example.final_project.BuildConfig
 import com.example.final_project.players.Data
+import com.example.final_project.players.Player
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
@@ -13,21 +13,20 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
-class APIFunctions(context: Context) {
+class APIFunctions(names: List<String>) {
 
 
 }
 
 fun listToString(arr: List<String>): String {
     var str = ""
-    for (item in arr) {
-        str += "$item,"
-    }
+    for (item in arr) str += "$item,"
     str.dropLast(1)
     return str
 }
 
-fun getPlayers(names:List<String>){
+private fun getPlayers(names: List<String>){
+    var players:List<Player>
     val interceptor = HttpLoggingInterceptor()
     interceptor.level =
         if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
@@ -50,19 +49,20 @@ fun getPlayers(names:List<String>){
 
     call.enqueue(object : Callback<Data> {
         override fun onFailure(call: Call<Data>, t: Throwable) {
-            //Toast.makeText(applicationContext, "FAIL что-то не так!", Toast.LENGTH_SHORT).show()
-
             Log.d("FAIL", "FAIL что-то не так!")
         }
 
-        override fun onResponse(call: Call<Data>, response: Response<Data>) {
-            //Toast.makeText(applicationContext, "Что-то заработало!", Toast.LENGTH_SHORT).show()
+        override fun onResponse(call: Call<Data>, response: Response<Data>){
             Log.d("OK", "Что-то заработало!")
             val data = response.body()
-            for(item in data!!.players){
-                Log.d("PLAYER_ID",item.id)
-                Log.d("PLAYER_NAME",item.attributes.name)
+            for (item in data!!.players) {
+                Log.d("PLAYER_ID", item.id)
+                Log.d("PLAYER_NAME", item.attributes.name)
             }
         }
     })
+}
+
+fun createPlayersList(data:Data):List<Player>{
+    return data.players
 }
