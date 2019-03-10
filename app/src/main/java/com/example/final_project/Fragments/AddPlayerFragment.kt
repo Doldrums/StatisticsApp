3fededac59452ccdfc2c7ec003584aa9f9461b5c
doldrums <doldrums.317@gmail.com>
2019-03-10 19:35:16 +0200

    fixed a bug with button exit

--- app/src/main/java/com/example/final_project/Fragments/AddPlayerFragment.kt
+++ app/src/main/java/com/example/final_project/Fragments/AddPlayerFragment.kt
@@ -3,15 +3,13 @@ package com.example.final_project.Fragments
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.final_project.API.APIFunctions
+import androidx.fragment.app.Fragment
 import com.example.final_project.MainActivity
 import com.example.final_project.MainActivity.Companion.LIST_PLAYERS_FRAGMENT
 import com.example.final_project.R
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.beginfragment_layout.*
 
 
@@ -30,13 +28,8 @@ class AddPlayerFragment : Fragment() {
         Log.d("FRAG", "Fragment BeginFragment started")
         val playerName = input_login.text.toString()
         btn_find.setOnClickListener {
-            if (playerName==""){
-                Snackbar.make(it,"Введите что-нибудь",Snackbar.LENGTH_LONG).show()
-            }else{
-                //тут надо наверное провернуть какие-то действия с апихой дабы проверить адекватно ли имя игрока
-                val mainActivity = this@AddPlayerFragment.activity as MainActivity
-                mainActivity.changeFragment(LIST_PLAYERS_FRAGMENT, playerName, "null")
-            }
+            val mainActivity = this@AddPlayerFragment.activity as MainActivity
+            mainActivity.changeFragment(LIST_PLAYERS_FRAGMENT, playerName, "null")
         }
     }
 
--- app/src/main/java/com/example/final_project/Fragments/ListPlayersFragment.kt
+++ app/src/main/java/com/example/final_project/Fragments/ListPlayersFragment.kt
@@ -2,22 +2,19 @@ package com.example.final_project.Fragments
 
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.final_project.Adapter
 import com.example.final_project.MainActivity
+import com.example.final_project.MainActivity.Companion.STAT_FRAGMENT
 import com.example.final_project.R
 import com.example.final_project.RecyclerItemClickListener
 import kotlinx.android.synthetic.main.listplayerfragment_layout.*
-import com.example.final_project.MainActivity.Companion.BEGIN_FRAGMENT
-import com.example.final_project.MainActivity.Companion.STAT_FRAGMENT
-
+import kotlinx.android.synthetic.main.name_player_item_layout.*
 
 
 private const val ARG_PARAM1 = "param1"
@@ -56,10 +53,6 @@ class ListPlayersFragment : Fragment() {
         my_recycler_view.layoutManager = LinearLayoutManager(activity!!.applicationContext)
         my_recycler_view.adapter = Adapter(players)
 
-//        addPlayer.setOnClickListener {
-//            val mainActivity = this@ListPlayersFragment.activity as MainActivity
-//            mainActivity.changeFragment(BEGIN_FRAGMENT)
-//        }
 
         my_recycler_view.addOnItemTouchListener(
             RecyclerItemClickListener(this@ListPlayersFragment.activity!!, my_recycler_view, object : RecyclerItemClickListener.OnItemClickListener {
