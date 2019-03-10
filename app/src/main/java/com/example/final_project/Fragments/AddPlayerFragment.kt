8067080b23c6b0d7f03bb169236595c7bb3f4197
doldrums <doldrums.317@gmail.com>
2019-03-10 17:17:21 +0200

    fixed Add Fragment

--- app/src/main/java/com/example/final_project/Fragments/AddPlayerFragment.kt
+++ app/src/main/java/com/example/final_project/Fragments/AddPlayerFragment.kt
@@ -7,9 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.final_project.API.APIFunctions
 import com.example.final_project.MainActivity
 import com.example.final_project.R
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.beginfragment_layout.*
 
 
@@ -26,13 +26,15 @@ class AddPlayerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.d("FRAG", "Fragment AddPlayerFragment started")
+        val playerName = input_login.text.toString()
         btn_find.setOnClickListener {
-
-            //получаем имя игрока и ищем через API
-
-            //Закрываем фрагмент и переходим к ListPlayersFragment
-            val ma = this@AddPlayerFragment.activity as MainActivity
-            ma.changeFragment(3, "типа Имя", "null")
+            if (playerName==""){
+                Snackbar.make(it,"Введите что-нибудь",Snackbar.LENGTH_LONG).show()
+            }else{
+                //тут надо наверное провернуть какие-то действия с апихой дабы проверить адекватно ли имя игрока
+                val ma = this@AddPlayerFragment.activity as MainActivity
+                ma.changeFragment(3, playerName, "null")
+            }
         }
     }
 
