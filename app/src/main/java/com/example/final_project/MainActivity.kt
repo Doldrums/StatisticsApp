b953d4299c0d964c6dcde6d8fca296b53377661e
doldrums <doldrums.317@gmail.com>
2019-03-11 13:00:17 +0200

    fixed issues

--- app/src/main/java/com/example/final_project/MainActivity.kt
+++ app/src/main/java/com/example/final_project/MainActivity.kt
@@ -26,19 +26,19 @@ public class MainActivity : AppCompatActivity() {
     }
     fun changeFragment(fragmentId : Int, name : String, id : String){
         when (fragmentId) {
-            1 -> supportFragmentManager
+            BEGIN_FRAGMENT -> supportFragmentManager
                 .beginTransaction()
                 .add(R.id.fragment, AddPlayerFragment.newInstance(), "frag1_tag")
                 .commit()
-            2 -> supportFragmentManager
+            STAT_FRAGMENT -> supportFragmentManager
                 .beginTransaction()
                 .add(R.id.fragment, StatFragment.newInstance(name, id), "frag2_tag")
                 .commit()
-            3 -> supportFragmentManager
+            LIST_PLAYERS_FRAGMENT -> supportFragmentManager
                 .beginTransaction()
                 .add(R.id.fragment, ListPlayersFragment.newInstance(name), "frag3_tag")
                 .commit()
-            4 -> supportFragmentManager
+            COMPARISON_FRAGMENT -> supportFragmentManager
                 .beginTransaction()
                 .add(R.id.fragment, ComparisonFragment.newInstance(), "frag4_tag")
                 .commit()
