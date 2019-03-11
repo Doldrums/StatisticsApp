c688cb2253200df1d8603012e59f257914d4f5b1
doldrums <doldrums.317@gmail.com>
2019-03-11 17:51:53 +0200

    add save

--- app/src/main/java/com/example/final_project/Fragments/AddPlayerFragment.kt
+++ app/src/main/java/com/example/final_project/Fragments/AddPlayerFragment.kt
@@ -7,9 +7,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.final_project.API.APIservice
+import com.example.final_project.BuildConfig
 import com.example.final_project.MainActivity
 import com.example.final_project.R
+import com.example.final_project.players.PlayerData
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.beginfragment_layout.*
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class AddPlayerFragment : Fragment() {
@@ -24,13 +35,49 @@ class AddPlayerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d("FRAG", "Fragment BeginFragment started")
         val playerName = input_login.text.toString()
-        Log.i("ooo_tag", playerName)
         btn_find.setOnClickListener {
-            Log.i("kek_tag","ghj")
-            val mainActivity = this@AddPlayerFragment.activity as MainActivity
-            mainActivity.changeFragment(MainActivity.LIST_PLAYERS_FRAGMENT, playerName, "null")
+
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.level =
+                if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build()
+
+
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .baseUrl("https://api.pubg.com/shards/steam/")
+                .build()
+
+
+            val apIservice = retrofit.create(APIservice::class.java)
+
+            val call = apIservice.getPlayer("CHEEL40000")
+
+            call.enqueue(object : Callback<PlayerData> {
+
+
+                override fun onFailure(call: Call<PlayerData>, t: Throwable) {
+                    Log.d("FAIL", "FAIL что-то не так!")
+                    Snackbar.make(it,"Something is wrong",Snackbar.LENGTH_LONG).show()
+                }
+
+
+                override fun onResponse(call: Call<PlayerData>, response: Response<PlayerData>) {
+                    Log.d("OK", "Что-то заработало!")
+                    val mainActivity = this@AddPlayerFragment.activity as MainActivity
+                    mainActivity.changeFragment(
+                        MainActivity.LIST_PLAYERS_FRAGMENT,
+                        response.body()!!.getName(),
+                        response.body()!!.getId())
+                }
+            })
+
+
         }
     }
 
