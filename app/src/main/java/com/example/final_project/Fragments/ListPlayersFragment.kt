82e60d3e644ef5c81a80371a6702285bbe3e0804
Philipp Maksimov <filipp39@gmail.com>
2019-08-09 15:03:21 +0200

    исправил корутины

--- app/src/main/java/com/example/final_project/Fragments/ListPlayersFragment.kt
+++ app/src/main/java/com/example/final_project/Fragments/ListPlayersFragment.kt
@@ -28,11 +28,16 @@ import kotlinx.android.synthetic.main.name_player_item_layout.view.*
 import kotlinx.coroutines.*
 
 
-class ListPlayersFragment() : Fragment() {
+class ListPlayersFragment : Fragment() {
     var players = mutableListOf<SimplePlayer>()
 
     private var name = ""
     private var id = ""
+
+    lateinit var db: PlayersDB
+    lateinit var adapter: PlayersAdapter
+
+    private val mainJob = SupervisorJob()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,45 +46,26 @@ class ListPlayersFragment() : Fragment() {
         return inflater.inflate(R.layout.listplayerfragment_layout, container, false)
     }
 
-    @ObsoleteCoroutinesApi
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        mainJob.start()
+
         //create database
-        val db = Room.databaseBuilder(
-            context!!,
-            PlayersDB::class.java, "playersDB"
-        ).build()
-
-        db.createDatabase(context!!)
-
-        my_recycler_view.layoutManager = LinearLayoutManager(activity!!.applicationContext)
-        val adapter = PlayersAdapter(players)
-        my_recycler_view.adapter = adapter
+        createDatabase()
+        initAdapter()
 
         //добавили ли мы игрока?
         val bundle = this.arguments
         if (bundle != null) {
             name = bundle.getString(NAME)!!
             id = bundle.getString(ID)!!
+
             if (bundle.getBoolean(ADD_PLAYER)) {
-                newSingleThreadContext("dBThread").use { GlobalScope ->
-                    runBlocking(GlobalScope) {
-                        cachePlayer(SimplePlayer(name, id), db)
-                        log("cashed SUCSESS: ${SimplePlayer(name, id)}")
-                        players.clear()
-                        players = db.simplePlayerDAO().getPlayersFromDB() as MutableList<SimplePlayer>
-                        adapter.setData(players)
-                    }
-                }
+                cachePlayers()
             }
         } else {
-            newSingleThreadContext("dBThread").use { GlobalScope ->
-                runBlocking(GlobalScope) {
-                    players = db.simplePlayerDAO().getPlayersFromDB() as MutableList<SimplePlayer>
-                    adapter.setData(players)
-                }
-            }
+            getPlayersFromDatabase()
         }
 
         adapter.setData(players)
@@ -116,16 +102,48 @@ class ListPlayersFragment() : Fragment() {
             val mainActivity = this@ListPlayersFragment.activity as MainActivity
             mainActivity.setFragment(PLAYER_ADDER)
         }
-
-
     }
-}
 
+    override fun onDestroyView() {
+        mainJob.cancel()
+        super.onDestroyView()
+    }
 
-fun cachePlayer(player: SimplePlayer, db: PlayersDB) {
-    GlobalScope.launch {
+    private fun initAdapter() {
+        my_recycler_view.layoutManager = LinearLayoutManager(activity!!.applicationContext)
+        adapter = PlayersAdapter(players)
+        my_recycler_view.adapter = adapter
+    }
+
+    private fun createDatabase() {
+        db = Room.databaseBuilder(context!!, PlayersDB::class.java, "playersDB").build()
+        db.createDatabase(context!!)
+    }
+
+    private fun getPlayersFromDatabase() = CoroutineScope(Dispatchers.Main + mainJob).launch {
+        val playersFromDb = withContext(Dispatchers.IO) { db.simplePlayerDAO().getPlayersFromDB() }
+
+        players.addAll(playersFromDb)
+        adapter.setData(players)
+    }
+
+    private fun cachePlayers() = CoroutineScope(Dispatchers.Main + mainJob).launch {
+        withContext(Dispatchers.IO) { cachePlayer(SimplePlayer(name, id)) }
+
+        log("cached SUCCESS: ${SimplePlayer(name, id)}")
+
+        players.clear()
+
+        val playersFromDb =
+            withContext(Dispatchers.IO) { db.simplePlayerDAO().getPlayersFromDB() }
+
+        players.addAll(playersFromDb)
+        adapter.setData(players)
+    }
+
+    private fun cachePlayer(player: SimplePlayer) {
         db.simplePlayerDAO().addPlayerToDB(player)
-        Log.d("DB", "Add ${player.toString()} to DB")
+        Log.d("DB", "Add $player to DB")
     }
 }
 
